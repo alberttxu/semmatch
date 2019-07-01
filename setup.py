d69aea35fff628002f79dd100d9c1491bc7faca6
Albert Xu <albert.t.xu@gmail.com>
2019-07-01 14:31:41 -0400

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="semmatch",
-    version="0.0.2",
+    version="0.0.3",
     author="Albert Xu",
     author_email="albert.t.xu@gmail.com",
     description="template matching tool for SerialEM",
